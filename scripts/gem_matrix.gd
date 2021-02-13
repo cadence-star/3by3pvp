7f3c1c794be3bde76c7e44a33577c260a7c425f4
Cadence✮ <137074cs@gmail.com>
2021-02-13 13:12:01 -0500

    add match checking when generating board

--- scripts/gem_matrix.gd
+++ scripts/gem_matrix.gd
@@ -10,7 +10,7 @@ var gemMatrix = [
 	[null,null,null,null,null,null,null,null],
 	[null,null,null,null,null,null,null,null],
 	[null,null,null,null,null,null,null,null]]
-var gems = [
+var gemTypes = [
 	preload("res://scenes/gems/green_gem.tscn"),
 	preload("res://scenes/gems/red_gem.tscn"),
 	preload("res://scenes/gems/yellow_gem.tscn"),
@@ -30,8 +30,40 @@ func _ready():
 #	pass
 
 func fill_matrix():
-	for i in 8:
-		for j in 8:
-			var gem = gems[(randi()%7)].instance()
+	for row in 8:
+		for column in 8:
+			var gemType = randi()%7
+			while has_match(gemType, row, column):
+				gemType = randi()%7
+			var gem = gemTypes[gemType].instance()
 			add_child(gem)
-			gemMatrix[i][j] = gem
+			gemMatrix[row][column] = gem
+
+func has_match(gemType, row, column):
+	return (
+		(
+			row > 1 and
+			gemMatrix[row-1][column] and
+			gemMatrix[row-2][column] and
+			gemMatrix[row-1][column].type == gemType and
+			gemMatrix[row-2][column].type == gemType
+		) or (
+			column > 1 and
+			gemMatrix[row][column-1] and
+			gemMatrix[row][column-2] and
+			gemMatrix[row][column-1].type == gemType and
+			gemMatrix[row][column-2].type == gemType
+		) or (
+			row < 6 and
+			gemMatrix[row+1][column] and
+			gemMatrix[row+2][column] and
+			gemMatrix[row+1][column].type == gemType and
+			gemMatrix[row+2][column].type == gemType
+		) or (
+			column < 6 and
+			gemMatrix[row][column+1] and
+			gemMatrix[row][column+2] and
+			gemMatrix[row][column+1].type == gemType and
+			gemMatrix[row][column+2].type == gemType
+		)
+	)
